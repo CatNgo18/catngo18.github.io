5a299dc3038bdfb73ac7b28876304cb7a7f57ad4
CatNgo18 <cattien.nhu.ngo@gmail.com>
2024-04-12 13:01:14 -0700

    contact links

--- src/components/Sections/Contact/index.tsx
+++ src/components/Sections/Contact/index.tsx
@@ -1,7 +1,13 @@
+import Link from "next/link"
+
 function Contact() {
   return (
     <div id='contact-section'>
       <h2>Contact</h2>
+      <div className="flex flex-row gap-10 justify-center">
+        <Link href="https://www.linkedin.com/in/cattien-ngo/">LinkedIn</Link>
+        <Link href="https://github.com/CatNgo18">GitHub</Link>
+      </div>
     </div>
   )
 }
