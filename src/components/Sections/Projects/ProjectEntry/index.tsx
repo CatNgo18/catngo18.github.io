fcbf47ff425872d4bb4c7a0d34360c8269cadfd4
CatNgo18 <cattien.nhu.ngo@gmail.com>
2024-04-17 12:14:14 -0700

    projects styling

--- src/components/Sections/Projects/ProjectEntry/index.tsx
+++ src/components/Sections/Projects/ProjectEntry/index.tsx
@@ -5,7 +5,7 @@ import { Project } from "..";
 
 export const ProjectEntry: React.FunctionComponent<Project> = props => {
     return (
-        <div className="text-left w-3/5">
+        <div className="text-left w-3/5 rounded-3xl bg-secondary/25 p-5 pb-1 m-2">
             <h3>{props.title}</h3>
             <h5>{`${props.company} | ${props.startDate}${props.endDate && `- ${props.endDate}`}`}</h5>
             <Bullets parent={props.title} bullets={props.responsibilities}/>
