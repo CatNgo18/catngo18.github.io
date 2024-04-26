c62098fa4c03fd0682d0cdebb0c4609263b24065
CatNgo18 <cattien.nhu.ngo@gmail.com>
2024-04-26 16:19:09 -0700

    add space below skill tags

--- src/components/Skills/index.tsx
+++ src/components/Skills/index.tsx
@@ -5,7 +5,7 @@ interface Prop {
 
 export const Skills: React.FunctionComponent<Prop> = props => {
     return (
-        <div className="flex flex-row gap-3 m-3 overflow-x-auto">
+        <div className="flex flex-row gap-3 m-3 mb-2 pb-1 overflow-x-auto">
             {props.skills.map((skill: string, index: number) => 
                     <div
                         className="grow-0 shrink-0 bg-secondary/50 text-text/75 font-medium px-4 py-1 rounded-3xl"
