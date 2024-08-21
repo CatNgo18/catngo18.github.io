e730c05dba9a385b3290bba5918e7790c833847e
CatNgo18 <cattien.nhu.ngo@gmail.com>
2024-08-20 18:27:19 -0700

    update main

--- src/components/Sections/Main/index.tsx
+++ src/components/Sections/Main/index.tsx
@@ -4,8 +4,8 @@ function Main() {
             <h1 className="font-shirkhand">
                 Cattien Ngo
             </h1>
-            <p className="font-bold">
-                Full Stack Software Engineer and Web Developer
+            <p className="font-bold italic text-lg">
+                Full Stack Software Engineer, UI/UX Enthusiast
             </p>
         </div>
     );
