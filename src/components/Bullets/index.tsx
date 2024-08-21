76335b62b6f6b305170337b1a0a7df706c1e386f
CatNgo18 <cattien.nhu.ngo@gmail.com>
2024-08-20 18:26:21 -0700

    update bullets

--- src/components/Bullets/index.tsx
+++ src/components/Bullets/index.tsx
@@ -9,7 +9,11 @@ export const Bullets: React.FunctionComponent<Prop> = props => {
     return (
         <ul className="list-disc ml-5 text-left text-text text-base">
             {props.bullets.map((desc: string, index: number) => 
-                    <li key={`${props.parent.replace(' ', '-')}-bullet-${index}`}>{parse(desc)}</li>
+                    <li key={`${props.parent.replace(' ', '-')}-bullet-${index}`}>
+                        <p>
+                            {parse(desc)}
+                        </p>
+                    </li>
                 )
             }
         </ul>
