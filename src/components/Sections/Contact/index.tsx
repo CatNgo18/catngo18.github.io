16c05b4239a39e59c8ebe816d86b11c964b5f967
CatNgo18 <cattien.nhu.ngo@gmail.com>
2024-08-20 18:26:49 -0700

    update contact

--- src/components/Sections/Contact/index.tsx
+++ src/components/Sections/Contact/index.tsx
@@ -1,11 +1,82 @@
 'use client'
 
-import {Github, Linkedin} from '@styled-icons/bootstrap';
+import { FormEvent, useRef, useState } from 'react';
+import { Button, Input, Textarea } from '../../../app/MTailwind';
+import { Github, Linkedin } from '@styled-icons/bootstrap';
+import emailjs from '@emailjs/browser';
 
 function Contact() {
+  const form = useRef(null);
+  const [emailStatus, setEmailStatus] = useState<'unsent' | 'loading' | 'sent' | 'error'>('unsent');
+
+  const sendEmail = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    if (process.env.NEXT_PUBLIC_EMAIL_SERVICE_ID
+      && process.env.NEXT_PUBLIC_EMAIL_TEMPLATE_ID
+      && process.env.NEXT_PUBLIC_EMAIL_PUBLIC_KEY
+      && form.current) {
+        setEmailStatus('loading');
+
+        emailjs
+          .sendForm(
+            process.env.NEXT_PUBLIC_EMAIL_SERVICE_ID,
+            process.env.NEXT_PUBLIC_EMAIL_TEMPLATE_ID,
+            form.current,
+            {
+              publicKey: process.env.NEXT_PUBLIC_EMAIL_PUBLIC_KEY,
+            }
+          )
+          .then(
+            () => {
+              setEmailStatus('sent');
+            },
+            () => {
+              setEmailStatus('error');
+            }
+          )
+    } else {
+      setEmailStatus('error');
+    }
+  }
   return (
-    <div id='contact-section' className="pb-16 mt-16 bg-secondary/50">
-      <div className="flex flex-row gap-10 justify-center mt-10">
+    <div id='contact-section' className="pb-16 mt-16 bg-background-dark/50 w-full">
+      <h2>Let&#39;s Talk!</h2>
+      <form ref={form} onSubmit={sendEmail} className='w-2/3 max-w-2xl mx-auto flex flex-col gap-2 items-center mt-5'>
+        <Input
+          label='Name'
+          name='user_name'
+        />
+        <Input
+          label='Email'
+          name='user_email'
+        />
+        <Textarea
+          label='Message'
+          name='message'
+        />
+        {emailStatus === 'unsent' || emailStatus === 'error' ?
+          <Button type='submit' disabled>
+            Send
+          </Button>
+        : emailStatus === 'loading' ?
+          <Button loading={true}>
+            Loading...
+          </Button>
+        : emailStatus === 'sent' ?
+          <Button type='submit' color='green'>
+            Sent!
+          </Button>
+        :
+        ''
+        }
+        {emailStatus === 'error' &&
+        <p>
+          Could not send your message. Please try again later.
+        </p>
+        }
+      </form>
+      <div className="flex flex-row gap-10 mt-10 justify-center">
         <Linkedin
           size={32}
           onClick={() => window.open("https://www.linkedin.com/in/cattien-ngo/")}
@@ -20,5 +91,5 @@ function Contact() {
     </div>
   )
 }
- 
+
 export default Contact
