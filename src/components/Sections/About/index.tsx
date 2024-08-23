b851d1ccc44f186548308f721a3b55a2232863be
CatNgo18 <cattien.nhu.ngo@gmail.com>
2024-08-22 23:46:55 -0700

    my image name

--- src/components/Sections/About/index.tsx
+++ src/components/Sections/About/index.tsx
@@ -2,7 +2,7 @@
 
 import Image from "next/image";
 import Link from "next/link";
-import me from '../../../../public/me.jpg'
+import me from '../../../../public/Me.jpg'
 
 function About() {
     return (
